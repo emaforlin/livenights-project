81922e343afa93c687b76a408ddbce19d35878fe
Ema Forlin <emaa.forlin@gmail.com>
2024-12-06 00:09:38 -0300

    improves the navbar styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <Image
-            aria-hidden
-            src="/logo.svg"
-            alt="File icon"
-            width={700}
-            height={500}
-          />
+        <Image
+          aria-hidden
+          src="/logo.svg"
+          alt="Brand logo with slogan: Where events come to life"
+          width={700}
+          height={500}
+        />
       </main>
     </div>
   );
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,15 +11,16 @@ const NavBar: React.FC<NavbarProps> = ({ items }) => {
 
 
     return (
-    <div className=" py-4 px-2 bg-blue-700 ">
-        <ul className="flex gap-6 justify-end text-xl text-gray-100">
-            {items.map((item, index) => (
-                <li key={index}>
-                    <Link href={item.href} className="hover:underline">{item.label}</Link>
-                </li>
+      <div className="z-40 sticky top-0 shadow-md">
+        <nav className="py-6 px-4 bg-blue-700 ">
+          <ul className="flex gap-6 justify-items-center justify-end text-xl text-gray-100">
+            <Link href="/" className="absolute left-4 font-bold text-3xl">LIVENIGHTS</Link>
+            {items.map((item) => (
+              <Link href={item.href} className="hover:underline">{item.label}</Link>
             ))}
-        </ul>
-    </div>    
+          </ul>
+        </nav>    
+      </div>   
   )
 }
 

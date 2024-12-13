4a40f6d62f510e532f3eebf07cadc548973ff823
Ema Forlin <emaa.forlin@gmail.com>
2024-12-13 14:33:16 -0300

    adds an alert when the sign up fails

--- components/SignupForm.tsx
+++ components/SignupForm.tsx
@@ -4,29 +4,46 @@ import React, { useEffect, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 const SignupForm = () => {
-  const { register, handleSubmit, reset } = useForm<UserSignupData>();
+  const { register, handleSubmit, reset, formState: {errors} } = useForm<UserSignupData>();
 
-  const [apiResp, setApiResp] = useState<string|null>(null);
+  const [apiResp, setApiResp] = useState<{
+    message: string,
+    isAnError: boolean
+  }|null>(null);
 
-  
+  const resetApiResp = () => {
+    setApiResp({
+      message: "",
+      isAnError: false,
+    });
+  }
 
   const onSubmit: SubmitHandler<UserSignupData> = async (data) =>  {
     try {
-      const resp = await fetch("/api/users", {
+        const resp = await fetch("/api/users", {
         method: "POST",
         headers: {"Content-Type":"application/json"},
         body: JSON.stringify(data),
       });
 
       const result = await resp.json();
-
+      console.log("RESULT: ", JSON.stringify(result))
       if (resp.ok) {
-        setApiResp(result.message || "Inicio de sesion exitoso!");
+        setApiResp({
+          message: "Registro exitoso!",
+          isAnError: false,
+        });
       } else {
-        setApiResp(result.message || "Inicio de sesion fallido.");
+        setApiResp({
+          message: result.message || "Registro fallido.",
+          isAnError: true,
+        });
       }
     } catch (err) {
-      setApiResp("Ocurrio un error. Intente nuevamente.");
+      setApiResp({
+        message: "Ocurrio un error. Intente nuevamente.",
+        isAnError: true,
+      });
     }
   };
   
@@ -39,40 +56,54 @@ const SignupForm = () => {
 
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="mx-6">
-      <div className="mt-4">
-        <label htmlFor="firstname" className="text-black text-lg">Nombre</label>
-        <input type="text" id="firstname" placeholder="Nombre" 
-        className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-        {...register("firstname", {required: true, maxLength: 80})}
-        />
-      </div>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="mx-6">
+        <div className="mt-4">
+          <label htmlFor="firstname" className="text-black text-lg">Nombre</label>
+          <input type="text" id="firstname" placeholder="Nombre" 
+          className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+          {...register("firstname", {required: true, maxLength: 80})}
+          />
+          {errors.firstname && <p className="text-red-600">Campo requerido</p>}
+        </div>
 
-      <div className="mt-4">
-        <label htmlFor="lastname" className="text-black text-lg">Apellido</label>
-        <input type="text" id="lastname" placeholder="Apellido" className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-        {...register("lastname", {required: true, maxLength: 100})}
-        />
-      </div>
+        <div className="mt-4">
+          <label htmlFor="lastname" className="text-black text-lg">Apellido</label>
+          <input type="text" id="lastname" placeholder="Apellido" className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+          {...register("lastname", {required: true, maxLength: 100})}
+          />
+          {errors.lastname && <p className="text-red-600">Campo requerido</p>}
+        </div>
 
-      <div className="mt-4">
-        <label htmlFor="email" className="text-black text-lg">Email</label>
-        <input type="text" id="email" placeholder="Email" className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black" 
-        {...register("email", {required: true, pattern: /^\S+@\S+$/i})}
-        />
-      </div>
+        <div className="mt-4">
+          <label htmlFor="email" className="text-black text-lg">Email</label>
+          <input type="text" id="email" placeholder="Email" className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black" 
+          {...register("email", {required: true, pattern: /^\S+@\S+$/i})}
+          />
+          {errors.email && <p className="text-red-600">Campo requerido</p>}
+        </div>
 
-      <div className="mt-4 mb-16">
-        <label htmlFor="password" className="text-black text-lg">Contraseña</label>
-        <input type="password" id="password" placeholder="Contraseña" className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black focus:tracking-widest"
-        {...register("password", {required: true, min: 8})}
-        />
-      </div>
+        <div className="mt-4 mb-16">
+          <label htmlFor="password" className="text-black text-lg">Contraseña</label>
+          <input type="password" id="password" placeholder="Contraseña" className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black focus:tracking-widest"
+          {...register("password", {required: true, min: 8})}
+          />
+          {errors.password && <p className="text-red-600">Campo requerido</p>}
+        </div>
+        <div className="w-full mt-4">
+          <button className="w-full h-12 font-bold text-2xl bg-blue-600 hover:bg-blue-800 transition duration-300">Registrarse</button>
+        </div>
+      </form>
 
-      <div className="w-full mt-4">
-        <button className="w-full h-12 font-bold text-2xl bg-blue-600 hover:bg-blue-800 transition duration-300">Registrarse</button>
+      { apiResp?.isAnError && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-red-600 text-white w-96 h-36 rounded-md shadow-lg flex items-center justify-center relative">
+            <button className="absolute top-0 right-0 text-sm text-black hover:underline self-end mr-4 mt-2" onClick={() => resetApiResp()}>cerrar</button>
+            <p className="text-xl font-bold">{apiResp.message}</p>
+        </div>
       </div>
-    </form>
+      )}
+    </>
   );
 }
 

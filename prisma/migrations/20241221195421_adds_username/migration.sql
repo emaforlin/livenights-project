f23fb611f35964a973b0de7fb6777d06e74e6eda
Ema Forlin <emaa.forlin@gmail.com>
2024-12-21 16:59:04 -0300

    adds a username field to the User model

--- prisma/migrations/20241221195421_adds_username/migration.sql
+++ prisma/migrations/20241221195421_adds_username/migration.sql
@@ -1,12 +1,28 @@
 -- CreateTable
+CREATE TABLE `Event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `uid` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    `location` VARCHAR(191) NOT NULL,
+    `producer_id` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Event_title_key`(`title`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
     `firstname` VARCHAR(191) NOT NULL,
     `lastname` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_username_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -25,8 +41,12 @@ CREATE TABLE `UserRole` (
     `user_id` INTEGER NOT NULL,
     `role_id` INTEGER NOT NULL,
 
+    UNIQUE INDEX `UserRole_user_id_role_id_key`(`user_id`, `role_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Event` ADD CONSTRAINT `Event_producer_id_fkey` FOREIGN KEY (`producer_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `UserRole` ADD CONSTRAINT `UserRole_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;

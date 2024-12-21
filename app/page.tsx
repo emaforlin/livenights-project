87c26965cc6a03cc2e33d54df254ac2e2121256f
Ema Forlin <emaa.forlin@gmail.com>
2024-12-21 14:34:22 -0300

    implements a sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          aria-hidden
-          src="/logo.svg"
-          alt="Brand logo with slogan: Where events come to life"
-          width={700}
-          height={500}
-        />
-      </main>
-    </div>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <Image
+            aria-hidden
+            src="/logo.svg"
+            alt="Brand logo with slogan: Where events come to life"
+            width={700}
+            height={500}
+          />
+        </main>
+      </div>
   );
 }

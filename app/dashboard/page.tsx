2f05e8a45f2ace2110a932a88f78bf0b196ca23b
Ema Forlin <emaa.forlin@gmail.com>
2024-12-21 21:05:41 -0300

    Adds an admin panel

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { DataTable } from '@/components/ui/data-table';
+import React, { useEffect, useState } from 'react'
+import { columns, EventData } from './types/columns';
+import { EventsTable } from './components/EventsTable';
 
-const page = () => {
+const AdminDashboard = () => {
   return (
-    <div>page</div>
+    <div className="flex flex-col items-center">
+      <h1 className="text-center text-3xl text-black mb-4">Tus Eventos</h1>
+      <div className="flex justify-center w-full overflow-x-auto">
+        <div className="flex justify-center max-w-4xl w-full">
+          <EventsTable />
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default page
+export default AdminDashboard;
--- context/EventContext.tsx
+++ context/EventContext.tsx
@@ -5,21 +5,31 @@ import { useState, createContext, ReactNode, useEffect, useContext } from "react
 import { EventType } from "@/types/event";
 
 type EventContextType = {
+    producerId: string;
+    selectProducer: (producerId: number) => void;
     events: EventType[];
-    fetchEvents: () => Promise<void>;
+    fetchEvents: (producerId: string) => Promise<void>;
 }
 
 const EventContext = createContext<EventContextType | undefined>(undefined);
 
 export const EventProvider = ({ children }: { children: ReactNode }) => {
-    const [events, setEvents] = useState<EventType[]>([]);
+    const [producerId, setProducerId] = useState<string>("");
 
-    const fetchEvents = async () => {
+    const selectProducer = (producerId: number) => {
+        const id = String(producerId);
+        setProducerId(id);
+    }
+
+    const [events, setEvents] = useState<EventType[]>([]);
+    
+    const fetchEvents = async (producerId: string) => {
         try {
-            const res = await fetch("/api/events");
+            const res = await fetch(`/api/events?producer=${producerId}`);
             if (!res.ok) {
                 throw new Error("error fetching events");
             }
+
             const data = await res.json();
             setEvents(data);
         } catch (error) {
@@ -28,11 +38,12 @@ export const EventProvider = ({ children }: { children: ReactNode }) => {
     }
 
     useEffect(() => {
-        fetchEvents();
-    }, []);
+        fetchEvents(producerId);
+    
+    }, [producerId]);
 
     return (
-        <EventContext.Provider value={{events, fetchEvents}}>
+        <EventContext.Provider value={{events, fetchEvents, producerId, selectProducer}}>
             {children}
         </EventContext.Provider>
     )

f1548a3ca75566c614b93d3d916e5c75aa875d1c
Ema Forlin <emaa.forlin@gmail.com>
2024-12-30 20:20:21 -0300

    extends next auth types

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,9 +1,21 @@
-import { DefaultUser } from 'next-auth';
-declare module 'next-auth' {
+import { Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
     interface Session {
-        user?: DefaultUser & { id: string; role: string };
+        id: string;
+        role: string;
     }
-    interface User extends DefaultUser {
+
+    interface User {
+        id: string;
         role: string;
     }
 }
+
+declare module "next-auth/jwt" {
+    interface JWT {
+      id: string;
+      role: string;
+    }
+  }

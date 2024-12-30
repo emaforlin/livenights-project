caf32e7a336bea80d28435842017d9ac4e9feb26
Ema Forlin <emaa.forlin@gmail.com>
2024-12-30 16:54:48 -0300

    set the middleware to perform the role checks

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,33 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-export default NextAuth(authConfig).auth;
+// export default NextAuth(authConfig).auth;
 
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"]
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login).*)",]
+}
+
+export async function middleware(req: NextRequest) {
+    const protectedRoutes : { [path: string]: string[] } = {
+        "/dashboard": ["PRODUCER", "USER"],
+        "/path": ["ROLE"]
+    };
+    
+    const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+
+    if (token) {
+        const currentPath = req.nextUrl.pathname;
+    
+        const requiredRole = protectedRoutes[currentPath];
+    
+        const userRoles = (token.role as string).split(" ");
+    
+        requiredRole?console.log("path:", currentPath, "has:", userRoles, "requires:", requiredRole):console.log();
+    
+        if (requiredRole && !userRoles.some(rule => requiredRole.includes(rule))) {
+            return NextResponse.redirect(new URL("/", req.url))
+        }
+        return NextResponse.next();
+    }
+    return NextResponse.next();
 }

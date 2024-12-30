8a12828e4c6b2391d148d69f0e693655c7f8ed3a
Ema Forlin <emaa.forlin@gmail.com>
2024-12-30 20:23:05 -0300

    refactor middleware to perform user authentication

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,33 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-// export default NextAuth(authConfig).auth;
-
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login).*)"]
 }
 
 export async function middleware(req: NextRequest) {
-    const protectedRoutes : { [path: string]: string[] } = {
+    const accessRules : { [path: string]: string[] } = {
         "/dashboard": ["PRODUCER", "USER"],
         "/path": ["ROLE"]
     };
     
     const token = await getToken({ req, secret: process.env.AUTH_SECRET });
     const currentPath = req.nextUrl.pathname;
-    const requiredRole = protectedRoutes[currentPath];
-    
-    const userRoles = token?(token.role as string):"GUEST";
-    console.log("path: ", currentPath);
-    console.log("required roles: ", requiredRole);
+    const requiredRole = accessRules[currentPath];
+
+    if (!requiredRole) console.log(`No access rule found for ${currentPath}. Please check the AccessRules and add at least one if it's needed.`)
+
+    const userRoles = token?.role ? String(token.role) : "GUEST"; // Asegurarse de que sea cadena
+    requiredRole?console.log("required roles: ", requiredRole):console.log();
     console.log("user roles: ", userRoles);
-    
-        
-    if (requiredRole && userRoles && !userRoles.split(" ").some(rule => requiredRole.includes(rule))) {
-        return NextResponse.redirect(new URL("/", req.url))
+
+    if (
+        requiredRole &&
+        typeof userRoles === "string" &&
+        !userRoles.split(" ").some(rule => requiredRole.includes(rule))
+    ) {
+        return NextResponse.redirect(new URL("/", req.url));
     }
-    
+
     return NextResponse.next();
 }

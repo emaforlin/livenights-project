697059ecd66b701fae61bcd7db06610c36c2cd40
Ema Forlin <emaa.forlin@gmail.com>
2025-01-14 02:07:00 -0300

    get the user role from the context

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,20 +1,15 @@
 import * as React from "react"
-import {
-  Ticket,
-} from "lucide-react"
 
 import { NavMain } from "@/components/nav-main"
 import { NavUser } from "@/components/nav-user"
-import { EventSwitcher } from "@/components/event-switcher"
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarRail,
 } from "@/components/ui/sidebar"
-import { SessionProvider, useSession } from "next-auth/react"
 import SigninBtn from "./auth/SigninBtn"
-import { getSession, getUserRoles } from "@/app/lib/dal"
+import { getSession, getUserRole } from "@/app/lib/dal"
 import { RoleProvider } from "@/context/RoleContext"
 
 
@@ -29,16 +24,15 @@ const data = {
 
 export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {  
   const session = await getSession();
-  const roles = await getUserRoles();
+  const role = await getUserRole();
   return (
-    <RoleProvider roles={roles}>
+    <RoleProvider role={role}>
       <Sidebar collapsible="icon" {...props}>
         <SidebarContent>
           <NavMain />
         </SidebarContent>
         <SidebarFooter>
           { session && (<NavUser user={data.user} />) || (<SigninBtn/>)}
-          
         </SidebarFooter>
         <SidebarRail />
       </Sidebar>

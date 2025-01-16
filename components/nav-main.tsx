338808ce10e3b77148ba404d9273d67489b2f72c
Ema Forlin <emaa.forlin@gmail.com>
2025-01-16 15:02:54 -0300

    adds user routes and nav items

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronRight, LayoutDashboard, Ticket, UserPen, type LucideIcon } from "lucide-react"
+import { ChevronRight, Compass, LayoutDashboard, Search, Ticket, UserPen, type LucideIcon } from "lucide-react"
 
 import {
   Collapsible,
@@ -45,28 +45,66 @@ const adminItems = [
   },
 ]
 
-const guestItems = [
+const userItems = [
   {
-    title: "Eventos",
-    url: "/dashboard",
-    icon: LayoutDashboard,
-    items: [
-      {
-        title: "Ventas",
-        url: "#"
-      }
-    ]
-  },
+    title: "Buscar eventos",
+    url: "/events",
+    icon: Search,
+  }, {
+    title: "Mis Tickets",
+    url: "/user/tickets",
+    icon: Ticket
+  }
 ]
 
 export function NavMain() {
-  const {roles} = useRole();
+  
   return (
     <SidebarGroup>
       <PermissionWrapper allowedRoles={["PRODUCER"]}> 
-        <SidebarGroupLabel>Admin Role</SidebarGroupLabel>
+        <SidebarGroupLabel>Seccion Productor</SidebarGroupLabel>
         <SidebarMenu>
           {adminItems.map((item) => (
+            <Collapsible
+              key={item.title}
+              asChild
+              className="group/collapsible"
+            >
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton tooltip={item.title}>
+                    {item.icon && <item.icon />}
+                    <Link href={item.url}>
+                      <span>{item.title}</span>
+                    </Link>
+                    {item.items && (<ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />)}
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+                {item.items && (
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {item.items?.map((subItem) => (
+                        <SidebarMenuSubItem key={subItem.title}>
+                          <SidebarMenuSubButton asChild>
+                            <Link href={subItem.url}>
+                              <span>{subItem.title}</span>
+                            </Link>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                )}
+              </SidebarMenuItem>
+            </Collapsible>
+            ))}
+        </SidebarMenu>
+      </PermissionWrapper>
+
+      <PermissionWrapper allowedRoles={["USER"]}>
+      <SidebarGroupLabel>Seccion Usuario</SidebarGroupLabel>
+        <SidebarMenu>
+          {userItems.map((item) => (
             <Collapsible
               key={item.title}
               asChild

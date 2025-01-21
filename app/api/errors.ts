73e21c9ba8768128d301a8d57b971424b9c9e568
Ema Forlin <emaa.forlin@gmail.com>
2025-01-21 14:41:49 -0300

    add allowed errors

---
+++ app/api/errors.ts
@@ -0,0 +1,9 @@
+export const ErrorSoldOut = new Error("there are no tickets in that batch");
+
+export const ErrorEventNotFound = new Error("event not found");
+
+export const ErrorBatchNotFound = new Error("could not retrive batch")
+
+export const ErrorPaymentFailure = new Error("payment failed");
+
+export const ErrorParsingBody = new Error("error parsing body");

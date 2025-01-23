b2bc09e7d91aa7d903f036df391be1be59c0926c
Ema Forlin <emaa.forlin@gmail.com>
2025-01-23 03:13:26 -0300

    fix typo on users api endpoint

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,16 +1,16 @@
-import { db } from "@/app/lib/db";
+import { prisma } from "@/app/lib/db";
 import { ErrorResponse, GenericResponse } from "@/utils/responses";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
     const userEmail = req.nextUrl.searchParams.get("email");
     if (userEmail) {
-        const user = await db.user.findUnique({where: {email: userEmail}});
+        const user = await prisma.user.findUnique({where: {email: userEmail}});
         if (!user) {
             return ErrorResponse("user not found", 404);
         }
         return GenericResponse(user, 200);
     }
-    const users = await db.user.findMany();
+    const users = await prisma.user.findMany();
     return GenericResponse(users, 200);
 }

298e02cea15bf5366017941bbfa79dc06d91c679
Ema Forlin <emaa.forlin@gmail.com>
2025-01-23 03:13:48 -0300

    adds an endpoint to fetch user's tickets

---
+++ app/api/users/[id]/tickets/route.ts
@@ -0,0 +1,23 @@
+import { ErrorUserNotFound } from "@/app/api/errors";
+import { prisma } from "@/app/lib/db";
+import { ErrorResponse, GenericResponse } from "@/utils/responses";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest, 
+    { params }: { params: Promise<{ id: string }> }
+) {
+    try {
+        const userId = (await params).id;
+        if (!userId) throw ErrorUserNotFound;
+        const userTickets = await prisma.ticketOrder.findMany({
+            where: {
+                user_id: parseInt(userId)
+            }
+        })
+
+        if (userTickets.length < 1) return ErrorResponse("no tickets found", 404);
+        return GenericResponse(userTickets, 200);
+    } catch (error: any) {
+        return ErrorResponse(error.message, 404);
+    }
+}

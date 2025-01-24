5c6e67183fe502e130f9d85b2643c685570e786f
Ema Forlin <emaa.forlin@gmail.com>
2025-01-24 19:22:12 -0300

    adds unauthorized page

---
+++ app/auth/unauthorized/page.tsx
@@ -0,0 +1,9 @@
+export function UnauthorizedPage() {
+    return (
+        <div className="flex w-full justify-center items-center">
+            <h1 className="font-bold text-4xl">No tienes permisos de acceso.</h1>
+        </div>
+    )
+}
+
+export default UnauthorizedPage;

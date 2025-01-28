1b57337d0ad49f2c0981d1d0241decfa37114f92
Ema Forlin <emaa.forlin@gmail.com>
2025-01-28 11:16:57 -0300

    Revert "remove auth routes from matcher"
    This reverts commit a8a20af6b1b129b452b6402e4d655d2f337fcdae.

--- middleware.ts
+++ middleware.ts
@@ -50,6 +50,7 @@ export async function middleware(req: NextRequest) {
 
 export const config =  {
     matcher: [
+        "/auth/:path*",
         "/dashboard/:path*",
         "/user/:path*",
         "/settings/:path*",

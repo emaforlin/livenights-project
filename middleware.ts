75c3943a6f288e7337bc6ff588c47e736d19727c
Ema Forlin <emaa.forlin@gmail.com>
2025-01-28 10:47:03 -0300

    more flexible with auth routes

--- middleware.ts
+++ middleware.ts
@@ -3,22 +3,34 @@ import { getToken } from "next-auth/jwt";
 import { getUserRole } from "./app/lib/dal";
 
 export async function middleware(req: NextRequest) {
+    if (
+        req.nextUrl.pathname.startsWith('/_next') ||
+        req.nextUrl.pathname.startsWith('/static') ||
+        req.nextUrl.pathname.startsWith('/favicon.ico')
+    ) {
+        return NextResponse.next();
+    }
+
     const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-    const role = await getUserRole()??"GUEST";
+    // const role = await getUserRole()??"GUEST";
+    const role = token?.role;
 
     const pathname = req.nextUrl.pathname
+    const isOnRoot = pathname === "/"
+    const isOnAuth = pathname.startsWith("/auth")
     const isOnDashboard = pathname.startsWith("/dashboard");
     const isOnSettings = pathname.startsWith("/settings");
     const isOnUserDashboard = pathname.startsWith("/user");
-    const isAuthAPIRoute = pathname.startsWith("/api/auth");
-    const isOnRegister = pathname.startsWith("/api/register");
+    // const isAuthAPIRoute = pathname.startsWith("/api/auth");
 
-    if (!token && !isAuthAPIRoute && !isOnRegister) {
-        return NextResponse.redirect(new URL("/auth/login", req.url));
+
+    if (!token && (isOnRoot || isOnAuth)) {
+        return NextResponse.next();
     }
 
-
-    // const role = (token?.role as string) || "GUEST";
+    if (!token) {
+        return NextResponse.redirect(new URL("/", req.url));
+    }
 
     if (isOnDashboard && role !== "PRODUCER") {
         return NextResponse.redirect(new URL("/", req.url));
@@ -29,17 +41,18 @@ export async function middleware(req: NextRequest) {
     }
 
     if (!token && isOnSettings) {
-        return NextResponse.redirect(new URL("/login", req.url));
+        return NextResponse.redirect(new URL("/auth/login", req.url));
     }
 
+    console.log(role, req.nextUrl.pathname);
     return NextResponse.next();   
 }
 
 export const config =  {
     matcher: [
-        // "/dashboard/:path*",
-        // "/settings/:path*",
-        // "/user/:path*",
-        // "/api/:path*"
+        "/auth/:path*",
+        "/dashboard/:path*",
+        "/user/:path*",
+        "/settings/:path*",
     ],
 };

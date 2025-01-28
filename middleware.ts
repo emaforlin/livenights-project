b93877054631ec55f211266dc16c97b52bb7f20a
Ema Forlin <emaa.forlin@gmail.com>
2025-01-27 23:23:05 -0300

    fetch the user role from the server side

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
+import { getUserRole } from "./app/lib/dal";
 
 export async function middleware(req: NextRequest) {
     const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+    const role = await getUserRole()??"GUEST";
+
     const pathname = req.nextUrl.pathname
     const isOnDashboard = pathname.startsWith("/dashboard");
     const isOnSettings = pathname.startsWith("/settings");
@@ -14,7 +17,7 @@ export async function middleware(req: NextRequest) {
         return NextResponse.redirect(new URL("/auth/login", req.url));
     }
 
-    const role = (token?.role as string) || "GUEST";
+    // const role = (token?.role as string) || "GUEST";
 
     if (isOnDashboard && role !== "PRODUCER") {
         return NextResponse.redirect(new URL("/", req.url));

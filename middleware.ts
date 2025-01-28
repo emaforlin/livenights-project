f5f8af76d6e829fe6134f77dec3cef1ac9f6d9c3
Ema Forlin <emaa.forlin@gmail.com>
2025-01-28 14:13:41 -0300

    fetch role from db

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-// import { getUserRole } from "./app/lib/dal";
+import { getUserRole } from "./app/lib/dal";
 
 export async function middleware(req: NextRequest) {
     const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-    const role = token?.role??"GUEST";
+    const role = await getUserRole()??"GUEST";
 
     const pathname = req.nextUrl.pathname
     const isOnDashboard = pathname.startsWith("/dashboard");

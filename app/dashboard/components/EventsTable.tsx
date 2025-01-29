3dfc8959bdf48961c8037d0bcd4e9d374937ab10
Ema Forlin <emaa.forlin@gmail.com>
2025-01-29 08:53:51 -0300

    fix error updating event

--- app/dashboard/components/EventsTable.tsx
+++ app/dashboard/components/EventsTable.tsx
@@ -24,35 +24,4 @@ export const EventsTable = () => {
         <DataTable columns={columns} data={summarizeEvents(events)} />
         </>
     );
-    // return (
-    //     <div className="w-full border rounded-lg overflow-hidden">
-    //       <table className="w-full">
-    //         <thead className="bg-gray-100 border-b">
-    //           <tr>
-    //             <th className="p-2 text-left">#</th>
-    //             <th className="p-2 text-left">Titulo</th>
-    //             <th className="p-2 text-left">Fecha</th>
-    //             <th className="p-2 text-left">Vendidos</th>
-    //             <th className="p-2 text-left"></th>
-    //             <th className="p-2 text-left"></th>
-    //           </tr>
-    //         </thead>
-    //         <tbody>
-    //           {events.map((event) => (
-    //             <tr key={event.id} className="border-b last:border-b-0 hover:bg-gray-50">
-    //               <td className="px-3">{event.id}</td>
-    //               <td className="px-3">{event.title}</td>
-    //               <td className="px-3">{new Date(event.date).toDateString()}</td>
-    //               <td className="px-3">{event._count.TicketOrder}</td>
-    //               <td><UpdateEventForm event={event}/></td>
-    //               <td className="px-3">
-    //                 <Button onClick={() => handleDelete(event.id)}
-    //                     variant="destructive">Eliminar</Button>
-    //               </td>
-    //             </tr>
-    //           ))}
-    //         </tbody>
-    //       </table>
-    //     </div>
-    //   );
     };
